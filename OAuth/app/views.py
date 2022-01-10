405ffbf9e3a0df83591450683d27d8055a2d5e94
Jamie Thompson <j.thomperoo@hotmail.com>
2022-01-10 13:33:32 +0000

    Fix incorrect README instructions, update NPM deps (#39)

--- OAuth/app/views.py
+++ OAuth/app/views.py
@@ -14,7 +14,6 @@
 
 from flask import Blueprint, render_template
 from qpylib import qpylib
-from . import rest
 
 qpylib.create_log()
 
@@ -22,10 +21,24 @@ qpylib.create_log()
 viewsbp = Blueprint('viewsbp', __name__, url_prefix='/')
 
 
+# A function that uses qpylib REST function to get a list of available Ariel databases.
+# The authentication entry in manifest.json will allow qpylib REST functions to handle the authentication for the app.
+def query_ariel_databases():
+    try:
+        response = qpylib.REST('get', '/api/ariel/databases')
+        qpylib.log('response=' + str(response.json()))
+        return response
+    except Exception as ex:
+        qpylib.log(
+            'Error calling REST api GET /api/ariel/databases: ' + str(ex),
+            'ERROR')
+        raise
+
+
 # An endpoint that gets a list of available Ariel databases by calling a python script which uses REST api call.
 @viewsbp.route('/getArielDatabases')
 def get_ariel_databases():
-    response = rest.get_ariel_databases()
+    response = query_ariel_databases()
     if response.status_code == 401:
         results = "A 'Deploy' is needed before this app can operate." \
                   "  Please navigate to 'Admin' tab and click 'Deploy Changes'."
