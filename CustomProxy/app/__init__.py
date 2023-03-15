205bbc6d21e90e02e061a02ab79f0dfecf5860d5
smokeykappa <64482575+smokeykappa@users.noreply.github.com>
2023-03-15 12:39:28 +0000

    Add custom proxy sample app (#45)

---
+++ CustomProxy/app/__init__.py
@@ -0,0 +1,82 @@
+# Copyright 2023 IBM Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+__author__ = 'IBM'
+
+import os
+
+from flask import Flask
+from flask_session import Session
+from flask_wtf.csrf import CSRFProtect
+from qpylib import qpylib
+from .proxy_utils import get_proxy_settings_from_json, set_proxy_env_variables
+
+
+# Flask application factory.
+def create_app():
+    # Create a Flask instance.
+    qflask = Flask(__name__)
+
+    # Initialize CSRF
+    csrf = CSRFProtect()
+    csrf.init_app(qflask)
+
+    # Retrieve QRadar app id.
+    qradar_app_id = qpylib.get_app_id()
+
+    # Create unique session cookie name for this app.
+    qflask.config['SESSION_COOKIE_NAME'] = f'session_{qradar_app_id}'
+
+    # Set up flask session
+    qflask.config["SESSION_PERMANENT"] = True
+    qflask.config["SESSION_TYPE"] = "filesystem"
+    Session(qflask)
+
+    if 'QRADAR_FLASK_SECRET_KEY' in os.environ:
+        qflask.config['SECRET_KEY'] = os.environ['QRADAR_FLASK_SECRET_KEY']
+    elif 'QRADAR_APP_UUID' in os.environ:
+        qflask.config['SECRET_KEY'] = os.environ['QRADAR_APP_UUID']
+
+    # Hide server details in endpoint responses.
+    # pylint: disable=unused-variable
+    @qflask.after_request
+    def obscure_server_header(resp):
+        resp.headers['Server'] = f'QRadar App {qradar_app_id}'
+        return resp
+
+    # Register q_url_for function for use with Jinja2 templates.
+    qflask.add_template_global(qpylib.q_url_for, 'q_url_for')
+
+    # Initialize logging.
+    qpylib.create_log()
+
+    # To enable app health checking, the QRadar App Framework
+    # requires every Flask app to define a /debug endpoint.
+    # The endpoint function should contain a trivial implementation
+    # that returns a simple confirmation response message.
+    @qflask.route('/debug')
+    def debug():
+        return 'Pong!'
+
+    # Import additional endpoints.
+    # For more information see:
+    #   https://flask.palletsprojects.com/en/1.1.x/tutorial/views
+    from . import views
+    qflask.register_blueprint(views.viewsbp)
+
+    # Set proxy settings if present in file in http_proxy and https_proxy environment variables
+    proxy_settings = get_proxy_settings_from_json()
+    set_proxy_env_variables(proxy_settings)
+
+    return qflask
---
+++ CustomProxy/app/views.py
@@ -0,0 +1,93 @@
+# Copyright 2023 IBM Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import requests
+
+from .proxy_utils import get_proxy_settings_from_json, get_proxy_settings_from_form
+from .proxy_utils import set_proxy_env_variables, write_proxy_settings_to_json
+from requests.exceptions import ConnectTimeout, ProxyError, SSLError
+from flask import Blueprint, render_template, request, redirect, session, url_for
+
+SAVE_SUCCESS_MESSAGE = 'Saved Successfully!'
+SAVE_FAILURE_MESSAGE = 'Save Failed!'
+
+PROXY_ERROR_MESSAGE = 'A proxy error occurred!'
+SSL_ERROR_MESSAGE = 'An SSL error occurred!'
+TIMEOUT_ERROR_MESSAGE = 'Timed out trying to connect to proxy!'
+
+SESSION_SAVE_STATUS = 'save_status'
+
+PROXY_HTML_TEMPLATE = 'proxy.html'
+REQUEST_TIMEOUT_IN_SECS = 2
+
+# pylint: disable=invalid-name
+viewsbp = Blueprint('viewsbp', __name__, url_prefix='/')
+
+
+@viewsbp.route('/')
+@viewsbp.route('/get_proxy_settings')
+def get_proxy_settings():
+    save_status = get_save_status_from_session()
+    proxy_settings = get_proxy_settings_from_json()
+    url = request.args.get('address')
+    if url:
+        return perform_url_request(url, proxy_settings, save_status)
+    return render_template(PROXY_HTML_TEMPLATE,
+                           proxy_settings=proxy_settings,
+                           save_status=save_status)
+
+
+@viewsbp.route('/save_proxy_settings', methods=['POST'])
+def save_proxy_settings():
+    proxy_settings = get_proxy_settings_from_form()
+    if write_proxy_settings_to_json(proxy_settings):
+        set_proxy_env_variables(proxy_settings)
+        session[SESSION_SAVE_STATUS] = SAVE_SUCCESS_MESSAGE
+    else:
+        session[SESSION_SAVE_STATUS] = SAVE_FAILURE_MESSAGE
+    return redirect(url_for('viewsbp.get_proxy_settings'), code=303)
+
+
+def get_save_status_from_session():
+    save_status = None
+    if SESSION_SAVE_STATUS in session:
+        save_status = session[SESSION_SAVE_STATUS]
+    session.pop(SESSION_SAVE_STATUS, None)
+    return save_status
+
+
+def process_url_response(proxy_settings, save_status, url, result,
+                         status_code):
+    return render_template(PROXY_HTML_TEMPLATE,
+                           proxy_settings=proxy_settings,
+                           save_status=save_status,
+                           url=url,
+                           result=result,
+                           status_code=status_code)
+
+
+def perform_url_request(url, proxy_settings, save_status):
+    try:
+        response = requests.get(url, timeout=REQUEST_TIMEOUT_IN_SECS)
+    except ProxyError as proxy_error:
+        return process_url_response(proxy_settings, save_status, url,
+                                    PROXY_ERROR_MESSAGE, str(proxy_error))
+    except SSLError as ssl_error:
+        return process_url_response(proxy_settings, save_status, url,
+                                    SSL_ERROR_MESSAGE, str(ssl_error))
+    except ConnectTimeout as timeout_error:
+        return process_url_response(proxy_settings, save_status, url,
+                                    TIMEOUT_ERROR_MESSAGE, str(timeout_error))
+    return process_url_response(proxy_settings, save_status, url,
+                                response.text, response.status_code)
